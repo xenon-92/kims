818e200e1baf19d5e8d4de3d934e1055d5df0565
Sumit Jana <sumit.jana@hyland.com>
2025-03-27 21:47:33 +0530

    added health check

--- main.go
+++ main.go
@@ -14,8 +14,13 @@ func main() {
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello Worlds")
 	})
+
 	router.HandleFunc("/employees", getEmployeesHandler).Methods(http.MethodGet)
+
+	router.HandleFunc("/health", healthCheckHandler).Methods(http.MethodGet)
+
 	http.ListenAndServe(":8080", router)
+
 	log.Println("Server started on port 8080")
 }
 
@@ -29,4 +34,8 @@ func getEmployeesHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(employeesJSON)
+}
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
