0edeb5e4121e1e7be6480a0663989e0e2c75e5f1
Sumit Jana <sumit.jana@hyland.com>
2025-07-30 12:48:08 +0530

    added startu probe

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+	time.Sleep(6 * time.Second) // Simulate a delay for startup probe check
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello Worlds")
 	})
